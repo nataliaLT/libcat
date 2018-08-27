91c505138e0181a5ac3f135cf06f290e08bd7ba7
natalia <natalia.lucerot@gmail.com>
2018-08-27 09:23:51 -0500

    primer commit

---
+++ cat-names/index.js
@@ -0,0 +1,8 @@
+// ejemplo var cats = requiere('cat-names');
+'use strict';
+var uniqueRandomArray = require('unique-random-array');
+var catNames = require('./cat-names.json');
+
+exports.all = catNames;
+exports.random = uniqueRandomArray(catNames);
+
